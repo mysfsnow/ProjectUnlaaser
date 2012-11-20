de4a3e0771086b944a2a926b5de0c1df8e9a8caa
RnMss <dreamfinal@gmail.com>
2012-11-21 01:02:50 +0800

    改变了解析方式
    见代码……

--- main.go
+++ main.go
@@ -16,14 +16,26 @@ func main() {
 		localAddr = ":80"
 	}
 
-	http.HandleFunc("/", HelloPage)
+	http.HandleFunc("/", handleMain)
 	err := http.ListenAndServe(localAddr, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
 
-func HelloPage(out http.ResponseWriter, request *http.Request) {
+func handleMain(out http.ResponseWriter, request *http.Request) {
+	u := (* request).URL;
+
+	log.Print("Requesting: ", u.Path)
+
+	if u.Path == "/" {
+		handleHello(out, request)
+	} else {
+		http.NotFound(out, request)
+	}
+}
+
+func handleHello(out http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(out, "<html><head><meta charset='utf-8' /><title>UnLaas</title></head><body>不能Laas</body></html>")
 }
 
