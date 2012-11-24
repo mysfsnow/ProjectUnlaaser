bafe06f1cb8e84b3227aef32efd6e74bd63fbc12
RnMss <dreamfinal@gmail.com>
2012-11-24 19:01:24 +0800

    增加了注释 增加了静态文件访问的功能

--- main.go
+++ main.go
@@ -1,36 +1,50 @@
 package main
 
 import (
-	"fmt"
+	"fmt" // 按格式输出字符串的库
 	"net/http"
 	"net/http/cgi"
 	"log"
 	"os"
+	"strings"
 )
 
+/** 主函数 */
 func main() {
 	args := os.Args;
 	var localAddr string
 	if len(args) == 2 {
+	/* 如果使用 一个参数 */
 		localAddr = args[1]
 	} else {
+	/* 不使用参数运行，默认使用 80 端口 */
 		localAddr = ":80"
 	}
 
-	http.HandleFunc("/", handleMain)
+	// 对于cgi路径下的网址，调用相应名称的cgi程序
 	http.HandleFunc("/cgi/", handleCgi)
+	// 对于stc路径下的网址，调用相应的静态文件
+	http.HandleFunc("/stc/", handleFile)
+	// 对于其他路径下的网址
+	http.HandleFunc("/", handleMain)
+
+	// 服务器启动
 	err := http.ListenAndServe(localAddr, nil)
+
+	// 当服务器出错退出时
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
 
 func handleMain (out http.ResponseWriter, request *http.Request) {
-	u := (* request).URL
+	u := (*request).URL
 
 	log.Print("Requesting: ", u.Path)
 
+	// 检查所访问的网址
 	switch u.Path {
+	// 根目录
 	case "/":
 		handleHello(out, request)
 	default:
@@ -38,16 +52,29 @@ func handleMain (out http.ResponseWriter, request *http.Request) {
 	}
 }
 
+/** 输出Hello的Handler */
 func handleHello(out http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(out, "<html><head><meta charset='utf-8' /><title>UnLaas</title></head><body>不能Laas</body></html>")
 }
 
+/** 调用CGI程序的Handler */
 func handleCgi(out http.ResponseWriter, request *http.Request) {
 	u := (* request).URL
-	
+
 	var hdl cgi.Handler
 	hdl.Path = "./" + u.Path
 	hdl.Root = "/cgi/"
 	hdl.Dir = "."
 	hdl.ServeHTTP(out, request)
+}
+
+/** 静态文件的Handler */
+func handleFile(out http.ResponseWriter, request *http.Request) {
+	u := (* request).URL
+	
+	if u.Path[:5] == "/stc/" {
+		http.ServeFile(out, request, u.Path[1:])
+	} else {
+		http.NotFound(out, request)
+	}
 }
